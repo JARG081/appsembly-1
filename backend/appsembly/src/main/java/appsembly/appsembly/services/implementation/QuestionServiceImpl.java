ea1ec7e0a14b0cab234f15ff361ea568fdfbc590
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:45:31 -0500

    Update QuestionServiceImpl.java
    comentarios question service

--- backend/appsembly/src/main/java/appsembly/appsembly/services/implementation/QuestionServiceImpl.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/implementation/QuestionServiceImpl.java
@@ -33,30 +33,33 @@ public class QuestionServiceImpl implements IQuestionService {
             // UserEntity user =
             // userRepository.findById(Long.parseLong(questionDTO.getUserID())).get();
             System.out.println("El assemblyID es: " + questionDTO.getAssemblyID());
-
+            // Obtener la asamblea asociada usando el ID de la asamblea proporcionado en el DTO de la pregunta
             AssemblyEntity assembly = assemblyRepository.findById(questionDTO.getAssemblyID()).get();
 
             System.out.println("El assembly es: " + assembly.getProposal());
+            // Crear un objeto de respuesta
             ResponseDTO response = new ResponseDTO();
-
+             // Crear una nueva entidad de pregunta y llenar sus campos con los datos del DTO
             QuestionEntity question = new QuestionEntity();
 
             question.setQuestionID(questionDTO.getQuestionID());
             question.setAssembly(assembly);
             System.out.println("La pregunta es: " + questionDTO.getQuestion());
             question.setQuestionText(questionDTO.getQuestion());
+            // Guardar la pregunta en el repositorio
             questionRepository.save(question);
 
             response.setMessage("Question create successfuly");
             return response;
         } catch (Exception e) {
+            // Manejar cualquier excepción lanzada durante el proceso y la volver a lanzar
             throw new Exception(e.getMessage());
         }
     }
-
+    // Implementación del método para obtener todas las preguntas de una asamblea específica
     @Override
     public List<QuestionEntity> findAllQuestions(String assemblyID) {
-
+        // Llama al método findAllQuestions del repositorio para obtener todas las preguntas de la asamblea
         return questionRepository.findAllQuestions(assemblyID);
     }
 }
