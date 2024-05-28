24fae7e901d828282c093fe1502ace4861e54e02
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:31:42 -0500

    Update IJWTUtilityService.java
    comentario service utility

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IJWTUtilityService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IJWTUtilityService.java
@@ -7,11 +7,14 @@ import java.text.ParseException;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jwt.JWTClaimsSet;
-
+// Definición de la interfaz IJWTUtilityService, que declara los métodos para generar y analizar JWTs
 public interface IJWTUtilityService {
+        // Método para generar un JWT. Recibe el ID de usuario y los roles como parámetros, y devuelve un String con el JWT generado.
+        // Puede lanzar varias excepciones relacionadas con la criptografía y la E/S.
         public String generateJWT(Long userId, String roles)
                         throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, JOSEException;
-
+        // Método para analizar un JWT. Recibe el JWT como parámetro y devuelve un JWTClaimsSet con los datos del token.
+        // Puede lanzar varias excepciones relacionadas con la criptografía, la E/S y el análisis del token.
         public JWTClaimsSet parseJwt(String jwt)
                         throws NoSuchAlgorithmException, InvalidKeySpecException, IOException, ParseException,
                         JOSEException;
