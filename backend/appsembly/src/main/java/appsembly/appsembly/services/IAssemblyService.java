acf401c6fe33875779d5eabcce7ba5a4bb96b1f4
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:28:11 -0500

    Update IAssemblyService.java
    comentario service assembly

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IAssemblyService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IAssemblyService.java
@@ -7,8 +7,10 @@ import appsembly.appsembly.services.models.dtos.AssemblyDTO;
 import appsembly.appsembly.services.models.dtos.ResponseDTO;
 
 public interface IAssemblyService {
+    // Método para registrar una nueva asamblea. Recibe un AssemblyDTO como parámetro y devuelve un ResponseDTO.
+    // Puede lanzar una excepción en caso de error.
     public ResponseDTO register(AssemblyDTO assembly) throws Exception;
-
+    // Método para obtener una lista de todas las asambleas. Devuelve una lista de AssemblyEntity.
     public List<AssemblyEntity> findAllAssemblies();
 
 }
