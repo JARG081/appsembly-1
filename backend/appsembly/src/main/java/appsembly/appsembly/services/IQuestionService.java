d985840b6026fa964614696b57ee13ced0dec2b6
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:32:38 -0500

    Update IQuestionService.java
    comentario questionservice

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IQuestionService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IQuestionService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import appsembly.appsembly.entities.QuestionEntity;
 import appsembly.appsembly.services.models.dtos.QuestionDTO;
 import appsembly.appsembly.services.models.dtos.ResponseDTO;
-
+// Definición de la interfaz IQuestionService, que declara los métodos relacionados con las preguntas
 public interface IQuestionService {
+    // Método para crear una nueva pregunta. Recibe un QuestionDTO como parámetro y devuelve un ResponseDTO.
+    // Puede lanzar una excepción en caso de error.
     public ResponseDTO create(QuestionDTO question) throws Exception;
-
+    // Método para obtener una lista de todas las preguntas asociadas a una asamblea específica.
+    // Recibe el ID de la asamblea como parámetro y devuelve una lista de QuestionEntity.
     public List<QuestionEntity> findAllQuestions(String assemblyID);
 }
