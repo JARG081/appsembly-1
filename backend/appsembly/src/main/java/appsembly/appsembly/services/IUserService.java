9b6be2e71e9ad8cebc3ee10e30b7a62897629664
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:33:52 -0500

    Update IUserService.java
    comentario user service

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IUserService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IUserService.java
@@ -6,17 +6,21 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import appsembly.appsembly.services.models.dtos.ResponseDTO;
 import appsembly.appsembly.services.models.dtos.UserDTO;
-
+// Definición de la interfaz IUserService, que declara los métodos relacionados con la gestión de usuarios
 public interface IUserService {
-
+    // Método para obtener una lista de todos los usuarios. Devuelve una lista de UserDTO.
+    // Puede lanzar UsernameNotFoundException en caso de no encontrar ningún usuario.
     public List<UserDTO> findAllUsers() throws UsernameNotFoundException;
-
+    // Método para encontrar un usuario específico por su identificador. Devuelve un UserDTO.
+    // Puede lanzar UsernameNotFoundException en caso de no encontrar el usuario.
     public UserDTO findUser(String identifier) throws UsernameNotFoundException;
-
+    // Método para crear un nuevo usuario. Recibe un UserDTO con los datos del nuevo usuario y devuelve un ResponseDTO.
+    // Puede lanzar una excepción en caso de error.
     public ResponseDTO createUser(UserDTO createUserDTO) throws Exception;
-
+    // Método para actualizar un usuario existente. Recibe un UserDTO con los datos actualizados y devuelve un ResponseDTO.
     public ResponseDTO updateUser(UserDTO updateUserDTO);
-
+    // Método para eliminar un usuario por su código personal. Recibe el código personal como parámetro y devuelve un ResponseDTO.
+    // Puede lanzar una excepción en caso de error.
     public ResponseDTO deleteUser(String personalCode) throws Exception;
 
 }
