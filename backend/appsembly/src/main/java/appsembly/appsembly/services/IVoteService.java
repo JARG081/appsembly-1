1edac9687f119015b0f1d647a39514b2ae490228
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:34:41 -0500

    Update IVoteService.java
    comentarios vote service

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IVoteService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IVoteService.java
@@ -2,7 +2,8 @@ package appsembly.appsembly.services;
 
 import appsembly.appsembly.services.models.dtos.ResponseDTO;
 import appsembly.appsembly.services.models.dtos.VoteDTO;
-
+// Definición de la interfaz IVoteService, que declara los métodos relacionados con la votación
 public interface IVoteService {
+    // Método para registrar un voto. Recibe un VoteDTO como parámetro y devuelve un ResponseDTO.
     public ResponseDTO vote(VoteDTO voteDTO);
 }
