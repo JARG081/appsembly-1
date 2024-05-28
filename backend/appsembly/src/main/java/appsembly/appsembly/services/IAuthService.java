669f75fe0ad9f863862789a1a8c70d7aa6a879fb
Manuel Benavides <121974065+JARG081@users.noreply.github.com>
2024-05-27 23:30:11 -0500

    Update IAuthService.java
    comentario authservice

--- backend/appsembly/src/main/java/appsembly/appsembly/services/IAuthService.java
+++ backend/appsembly/src/main/java/appsembly/appsembly/services/IAuthService.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import appsembly.appsembly.entities.UserEntity;
 import appsembly.appsembly.services.models.dtos.LoginDTO;
 import appsembly.appsembly.services.models.dtos.ResponseDTO;
-
+// Definición de la interfaz IAuthService, que declara los métodos relacionados con la autenticación
 public interface IAuthService {
+    // Método para realizar el inicio de sesión. Recibe un LoginDTO como parámetro y devuelve un HashMap con información de autenticación.
+    // Puede lanzar una excepción en caso de error.
     public HashMap<String, String> login(LoginDTO loginRequest) throws Exception;
-
+    // Método para registrar un nuevo usuario. Recibe un UserEntity como parámetro y devuelve un ResponseDTO.
+    // Puede lanzar una excepción en caso de error.
     public ResponseDTO register(UserEntity user) throws Exception;
 }
